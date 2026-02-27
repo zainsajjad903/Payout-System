81a38c855211528189e25cb01b59101ac7a7ea51
zainsajjad903 <zainsajjad.903@gmail.com>
2026-02-27 17:08:19 +0500

    Updated design and fixed bugs

--- payout.js
+++ payout.js
@@ -4,6 +4,7 @@ let halfleave = Number(prompt("Enter the number of half leave days:"));
 let sickleave = Number(prompt("Enter the number of sick leave days:"));
 let extra = Number(prompt("Enter the extra amount:"));
 let ramdanbouspercent = Number(prompt("Enter the Ramadan bonus percentage:"));
+//extra information
 let daysinmonth = 30;
 let hoursperday = 8;
 //total salery days month or hour  ka lyhaz sa
@@ -12,8 +13,34 @@ let perhour = perday / hoursperday;
 let perminute = perhour / 60;
 //jo ubsent thin or half leaves thin un kly ha ya
 let unpaidDays = absent + halfleave * 0.5;
-let deduction = unpaidDays * perday;
+//leave logic
+if (sickLeave > 0) {
+  if (sickLeave <= 3) {
+  } else {
+    let extraSickDays = sickLeave - 3;
+    unpaidDays += extraSickDays;
+    alert(
+      "Only 3 sick leaves are paid. Extra " +
+        extraSickDays +
+        " day(s) deducted.",
+    );
+  }
+}
+let extramoney = 0;
+if (shift === "night") {
+  extramoney = 2000;
 
-// Net salary
-let ramadanBonus = (ramdanbouspercent / 100) * basesalery;
-let netSalary = basesalery - deduction + extra + ramadanBonus;
+  if (baseSalary >= 50000) {
+    extramoney += 1000;
+  }
+} else if (shift === "day") {
+  extramoney = 0;
+} else {
+  document.writeln("Invalid shift! Defaulting to day.");
+}
+let deduction = unpaidDays * perDay;
+let ramadanBonus = (bonusPercent / 100) * baseSalary;
+let netSalary = baseSalary - deduction + extra + ramadanBonus + extramoney;
+document.writeln(
+  `Per Day: ${perDay.toFixed(2)}\nPer Hour: ${perHour.toFixed(2)}\nPer Minute: ${perMinute.toFixed(2)}\nNet Salary: ${netSalary.toFixed(2)}`,
+);
