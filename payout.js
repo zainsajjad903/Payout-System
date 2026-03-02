34d31d12b465b28252c56e0fa27a6a53caaf1f61
zainsajjad903 <zainsajjad.903@gmail.com>
2026-03-02 11:08:22 +0500

    Updated design and fixed bugs

--- payout.js
+++ payout.js
@@ -5,7 +5,7 @@ let sickleave = Number(prompt("Enter the number of sick leave days:"));
 let extra = Number(prompt("Enter the extra amount:"));
 let ramdanbouspercent = Number(prompt("Enter the Ramadan bonus percentage:"));
 //extra information
-let daysinmonth = 30;
+let daysinmonth = 31;
 let hoursperday = 8;
 //total salery days month or hour  ka lyhaz sa
 let perday = basesalery / daysinmonth;
@@ -13,6 +13,12 @@ let perhour = perday / hoursperday;
 let perminute = perhour / 60;
 //jo ubsent thin or half leaves thin un kly ha ya
 let unpaidDays = absent + halfleave * 0.5;
+//invalid daays lgic
+if (daysinmonth > 31 || daysinmonth <= 0) {
+  window.alert(
+    "Invalid number of days in a month! Please enter a value less than or equal to 31.",
+  );
+}
 //leave logic
 if (sickleave > 0) {
   if (sickleave <= 3) {
