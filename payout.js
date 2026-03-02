609a12db7ba4815037322abe04a7b32dcebe834c
zainsajjad903 <zainsajjad.903@gmail.com>
2026-03-02 17:09:17 +0500

    Updated design and fixed bugs

--- payout.js
+++ payout.js
@@ -15,10 +15,10 @@ let perminute = perhour / 60;
 let unpaidDays = absent + halfleave * 0.5;
 //invalid daays lgic
 if (daysinmonth > 31 || daysinmonth <= 0) {
-  document.writeln(
-    "Invalid input! Number of days in the month must be between 1 and 31.",
-  );
-} else if (halfleave < 0 || sickleave < 0 || sickleave > daysinmonth) {
+ resultEl.innerHTML = "Invalid input";
+  return;
+  
+} else if (sickleave < 0 || sickleave > daysinmonth) {
   document.writeln(
     "Invalid input! Absent, half leave, and sick leave days must be between 0 and the number of days in the month.",
   );
@@ -29,9 +29,8 @@ if (daysinmonth > 31 || daysinmonth <= 0) {
     "Invalid input! Ramadan bonus percentage must be between 0 and 100.",
   );
 } else if (basesalery <= 0 || basesalery >= 1000000) {
-  document.writeln(
-    "Invalid input! Base salary must be between 0 and 1,000,000.",
-  );
+   resultEl.innerHTML = "Invalid input";
+  return;
 } else if (absent > daysinmonth || absent < 0) {
   document.writeln(
     "Invalid input! Absent or half leave days cannot exceed the number of days in the month.",
@@ -65,14 +64,19 @@ if (shift === "night") {
 } else if (shift === "day") {
   extramoney = 0;
 } else {
-  document.writeln("Invalid shift! Please enter 'day' or 'night'.");
+  resultEl.innerHTML = "Invalid shift! Please enter 'day' or 'night'.";
+  return;
 }
 //final salary calculation
 let deduction = unpaidDays * perday; //wo psy jo absent or half leave k waja sa cut hon gy
 let ramadanBonus = (ramdanbouspercent / 100) * basesalery; //rmazan ka bonas add ho ga
 let netSalary = basesalery - deduction + extra + ramadanBonus + extramoney; //final salary calculate ho ga
 const resultEl = document.getElementById("result");
-
+if (netSalary <= 0) {
+  resultEl.innerHTML =
+    "Net salary is 0 or negative after deductions. Please check the inputs.";
+  return;
+}
 resultEl.innerHTML = `
   <h5 class="mb-3 text-primary">Payroll Summary</h5>
   <ul class="list-group">
